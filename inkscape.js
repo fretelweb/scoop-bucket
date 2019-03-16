b9e1ad1087dde2cb9910300a7ee3cce1c79e355e
Ronny Fretel <ronny@fretelweb.com>
2019-03-16 11:11:39 -0500

    Update and rename inkscape.js to inkscapealpha.js

--- inkscape.js
+++
@@ -1,6 +0,0 @@
-{ 
-  version: "1.0 alpha",
-  url: "https://ci.appveyor.com/api/buildjobs/dc6dtbhe6se73pej/artifacts/build%2Finkscape-1.0alpha_2019-03-15_1602477-x64.7z",
-  extract_dir: "inkscape10",
-  bin: "inkscape.exe" 
-}
